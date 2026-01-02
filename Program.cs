4f56aac93a453bb44bea7c06921274dcb6c4e22e
nadezhkaungg <nadezhkaungg@gmail.com>
2026-01-02 10:09:17 +0200

    Task005 - ValidParentheses - Solution with Stack structure.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetcodeTasks;
 
-Task004LongestCommonPrefix.Run();
+Task005ValidParentheses.Run();
---
+++ Task005ValidParentheses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LeetcodeTasks
+{
+    internal class Task005ValidParentheses
+    {
+        public static void Run()
+        {
+            string input = "(){((})}{}";
+
+            ArgumentNullException.ThrowIfNull(input);
+
+            if (input.Length % 2 != 0)
+            {
+                Console.WriteLine (false);
+                return;
+            }
+
+            var stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine(false);
+                        return;
+                    }
+
+                    char open = stack.Pop();
+
+                    if ((c == ')' && open != '(') || (c == '}' && open != '{') || (c == ']' && open != '['))
+                    {
+                        Console.WriteLine (false);
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine(stack.Count == 0);
+        }
+    }
+}
