08674ac7f03fcd3247c428dbb5a233b9e1c3a75c
nadezhkaungg <nadezhkaungg@gmail.com>
2026-01-03 08:42:20 +0200

    "Index of the First Occurrence in a String" task with For  cicle.

---
+++ Task006IndexOfFirstOccurence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTasks
+{
+    internal class Task006IndexOfFirstOccurence
+    {
+        public static void Run()
+        {
+            string haystack = "sadbutsad";
+            string needle = "but";
+
+            ArgumentNullException.ThrowIfNull(haystack);
+            ArgumentNullException.ThrowIfNull(needle);
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack[i..(i + (needle.Length))] == needle)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+
+            Console.WriteLine(-1);
+        }
+    }
+}
