118f42cb0ffe37babd34c6d06057ca683ba41609
nadezhkaungg <nadezhkaungg@gmail.com>
2026-01-03 09:21:40 +0200

    Task007 -   Length of Last Word - with For cicle.

---
+++ Task007LengthOfLastWord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTasks
+{
+    internal class Task007LengthOfLastWord
+    {
+        public static void Run()
+        {
+            string s = "   fly me   to   the mooooon  ";
+        
+            ArgumentNullException.ThrowIfNull(s);
+            int count = 0;
+
+            for (int i = s.Length - 1; i > 0; i--)
+            {
+                if (s[i] != ' ' && s[i - 1] == ' ')
+                {
+                    break;
+                }
+
+                if (s[i] != ' ')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count + 1 );
+
+        }
+    }
+}
