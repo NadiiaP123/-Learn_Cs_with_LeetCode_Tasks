6488937880f10bc70a9794f1c99415efd88c1b70
nadezhkaungg <nadezhkaungg@gmail.com>
2026-01-04 13:28:34 +0200

    Task010ExcelSheetColumnTitle - use Unicode code point ('A' + offset).

---
+++ Task010ExcelSheetColumnTitle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTasks
+{
+    internal class Task010ExcelSheetColumnTitle
+    {
+        public static void Run()
+        {
+            int columnNumber = 1605; //BIS
+
+            var sb = new StringBuilder();
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                sb.Append((char)('A' + columnNumber % 26));
+                columnNumber /= 26;
+
+            }
+
+            Console.WriteLine(sb.ToString().Reverse().ToArray());
+
+
+       }
+    }
+}
