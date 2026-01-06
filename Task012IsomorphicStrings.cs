2e2281b852415ede4d49d89783cb2e7cb51134db
nadezhkaungg <nadezhkaungg@gmail.com>
2026-01-06 22:46:19 +0200

    Task012IsomorphicStrings - first try (bad result)

---
+++ Task012IsomorphicStrings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTasks
+{
+    internal class Task012IsomorphicStrings
+    {
+        public static void Run()
+        {
+            string s = "paper", t = "title"; //true
+            //string s = "foo", t = "bar"; //false
+
+            int[] sChanged = new int[s.Length];
+            int[] tChanged = new int[s.Length];
+            int digit = 1;
+
+            if (s.Length != t.Length)
+            {
+                Console.WriteLine(false);
+                return;
+            }
+
+         for (int i = 0; i < s.Length; i++)
+            {
+                char sSymb = s[i];
+                char tSymb = t[i];
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[j] == sSymb && sChanged[j] == 0)
+                    {
+                        sChanged[j] = digit;
+                    }
+
+                    if (t[j] == tSymb && tChanged[j] == 0)
+                    {
+                        tChanged[j] = digit;
+                    }
+
+                    if (sChanged[j] != tChanged[j])
+                    {
+                        Console.WriteLine(false);
+                        return;
+                    }
+                }
+                
+                digit++;
+            }
+
+            Console.WriteLine(string.Join(", ", sChanged));
+            Console.WriteLine(string.Join(", ", tChanged));
+            Console.WriteLine(true);
+        }
+    }
+}
