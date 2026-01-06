4aa06793e0f452c3f6d7311e67bc8901b4d7ee7c
nadezhkaungg <nadezhkaungg@gmail.com>
2026-01-06 19:48:24 +0200

    Task011ExcelSheetColumnNumber - Column Title to Column Number

---
+++ Task011ExcelSheetColumnNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTasks
+{
+    internal class Task011ExcelSheetColumnNumber
+    {
+        public static void Run()
+        {
+            string columnTitle = "ADO";  //795
+
+            int result = 0;
+
+            foreach (char i in columnTitle)
+            {
+                int number = i - 'A' + 1;
+                result = result * 26 + number;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
